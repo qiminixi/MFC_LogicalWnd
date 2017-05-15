99d994f1b352bda8acdf8e6f920134aec68b7783
qiminixi <qiminixi@163.com>
2017-05-15 21:36:17 +0800

    ignore *.user

--- LogicalWnd/---.sh
+++ LogicalWnd/---.sh
@@ -1,7 +1,10 @@
-set -xv
+echo ===============================
 git rm --cached *.user
+echo ===============================
 git add .
+echo ===============================
 git commit -m "ignore *.user"
+echo ===============================
 git push origin master
-
+echo ===============================
 read -p "Press any key to continue." var
